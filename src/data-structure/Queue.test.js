1185460f3068e4f51bf30b706ea019c5910ce4a2
이정주 <motivator@soblo.io>
2020-03-01 14:34:42 +0900

    udpate

--- src/data-structure/Queue.test.js
+++ src/data-structure/Queue.test.js
@@ -1,14 +1,17 @@
+import {assert} from 'chai';
+
+// Factory Pattern
 const Queue = (..._items) => {
 
   let reverse = false; // encapsulation variable..
   let items = [..._items]; // encapsulation variable..
 
   function enqueue(...addItems) {
-    return reverse
+    return reverse ? items.unshift(...addItems) : items.push(...addItems);
   }
 
   function dequeue() {
-    
+    return reverse ? items.pop() : items.shift();
   }
 
   function setReverse(_reverse) {
@@ -22,6 +25,41 @@ const Queue = (..._items) => {
 
   return {
     enqueue,
-    dequeue
+    dequeue,
+    setReverse,
+    getItems
   }
 };
+
+test('Queue 데이터 enqueue Test (Right IN Left Out)', (done) => {
+  const queue = Queue(1,2,3);
+  queue.enqueue(4,5);
+  assert.deepEqual(queue.getItems(), [1,2,3,4,5]);
+  done();
+});
+
+test('Queue 데이터 dequeue Test (Right IN Left Out)', (done) => {
+  const queue = Queue(1,2,3);
+  queue.dequeue();
+  assert.deepEqual(queue.getItems(), [2,3]);
+  done();
+});
+
+
+test('Queue 데이터 enqueue Test (Left In Right Out)', (done) => {
+  const queue = Queue(3,2,1);
+  queue.setReverse(true);
+  queue.enqueue(4,5);
+  assert.deepEqual(queue.getItems(), [4,5,3,2,1]);
+  done();
+});
+
+test('Queue 데이터 dequeue Test  (Left In Right Out)', (done) => {
+  const queue = Queue(3,2,1);
+  queue.setReverse(true);
+  queue.dequeue();
+  assert.deepEqual(queue.getItems(), [3,2]);
+  done();
+});
+
+
